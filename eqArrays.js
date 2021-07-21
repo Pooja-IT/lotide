d6497e7f17fed668d23b0510abe8e3f3ea1d94b8
Pooja Patel <poojapatel.web@gmail.com>
2021-07-21 13:51:11 -0400

    Implement equal Arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  let emoji1 = String.fromCodePoint(0x1F600);
+  if (actual === expected) {
+    //console.log(emoji1 + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${emoji1} Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    //console.log(emoji1 + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${emoji1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays =  function(a,b) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]) , true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
