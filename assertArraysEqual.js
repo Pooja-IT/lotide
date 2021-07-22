e280c8dc2471efecdbbbb6214a0b43d72f6c56cd
Pooja Patel <poojapatel.web@gmail.com>
2021-07-22 01:27:13 -0400

    Updated implement assertion array equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+/*const assertEqual = function(actual, expected) {
   let emoji1 = String.fromCodePoint(0x1F600);
   if (actual === expected) {
     //console.log(emoji1 + "Assertion Passed: " + actual + " === " + expected);
@@ -7,14 +7,27 @@ const assertEqual = function(actual, expected) {
     //console.log(emoji1 + "Assertion Failed: " + actual + " !== " + expected);
     console.log(`${emoji1} Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-const eqArrays =  function(a,b) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) return false;
+};*/
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]) , true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false 
+const assertArraysEqual = function(arr1,arr2){
+  let emoji1 = String.fromCodePoint(0x1F600);
+  let result = eqArrays(arr1,arr2)
+  if (!result){
+    console.log(`${emoji1} Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+  else {
+    console.log(`${emoji1} Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+  //console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+  //console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
