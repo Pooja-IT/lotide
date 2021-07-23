98eae8d57f140ef3f8734a9943dd568f232904a7
Pooja Patel <poojapatel.web@gmail.com>
2021-07-22 23:42:06 -0400

    equal Arrays

--- eqArrays.js
+++ eqArrays.js
@@ -23,3 +23,4 @@ console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
