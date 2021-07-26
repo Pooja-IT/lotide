a3a88e140f4297f545a99885ddc46fd1a4ee8a02
Pooja Patel <poojapatel.web@gmail.com>
2021-07-26 16:29:16 -0400

    Test file for assert equal

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// test/assertEqualTest.js
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
