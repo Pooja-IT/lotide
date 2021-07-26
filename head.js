64a81308ecfc8312d80b4fa2bf403222cc00d143
Pooja Patel <poojapatel.web@gmail.com>
2021-07-26 16:27:11 -0400

    refactored the code using Modularizing

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let emoji1 = String.fromCodePoint(0x1F600);
-  if (actual === expected) {
-    console.log(`${emoji1} Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`${emoji1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const head =  function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
