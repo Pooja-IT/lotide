eec1dca1dd222247f2c554a441d60a02169036bb
Pooja Patel <poojapatel.web@gmail.com>
2021-07-27 12:51:28 -0400

    refactored the code using Modularizing

--- middle.js
+++ middle.js
@@ -1,24 +1,6 @@
-const assertArraysEqual = function(arr1,arr2){
-  let emoji1 = String.fromCodePoint(0x1F600);
-  let result = eqArrays(arr1,arr2)
-  if (!result){
-    console.log(`${emoji1} Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-  else {
-    console.log(`${emoji1} Assertion Passed: ${arr1} === ${arr2}`);
-  }
-};
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+//const eqArrays = require('./eqArrays');
+//const assertArraysEqual = require('./assertArraysEqual');
+
 const middle = function(array) {
   let newArr = [];
     if (array.length <= 2 && array.length > 0){
@@ -32,9 +14,6 @@ const middle = function(array) {
     }
     return newArr;  
   };
- console.log(middle([1])); // => []
- console.log(middle([1, 2])); // => []
- console.log(middle([1, 2, 3])); // => [2]
- console.log(middle([1, 2, 3, 4, 5])); // => [3]
- console.log(middle([1, 2, 3, 4])); // => [2, 3]
- console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+  module.exports = middle;
+ 
