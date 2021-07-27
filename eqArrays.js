04431b2cc3bbcd99906e13624041b52870cf7eda
Pooja Patel <poojapatel.web@gmail.com>
2021-07-26 22:38:33 -0400

    refactored the code using Modularizing

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let emoji1 = String.fromCodePoint(0x1F600);
-  if (actual === expected) {
-    //console.log(emoji1 + "Assertion Passed: " + actual + " === " + expected);
-    console.log(`${emoji1} Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    //console.log(emoji1 + "Assertion Failed: " + actual + " !== " + expected);
-    console.log(`${emoji1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-<<<<<<< HEAD
-const eqArrays =  function(a,b) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
-};
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]) , true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-=======
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -31,9 +9,5 @@ const eqArrays = function(arr1,arr2) {
   }
   return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = eqArrays;
 
->>>>>>> backup
